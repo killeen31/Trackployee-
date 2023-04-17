39cb370f277915a73a2cedb669bdcc43492615d5
killeen31 <j31killeen@gmail.com>
2023-04-17 09:24:33 -0400

    schema.sql, ran inquirer, and put instructions into server.js

---
+++ db/schema.sql
@@ -0,0 +1,26 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  department_name VARCHAR(300) NOT NULL,
+  PRIMARY KEY (id)
+);
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(300) NOT NULL,
+  salary DECIMAL(10,2) NOT NULL,
+  department_id INT,
+  FOREIGN KEY (department_id) 
+  REFERENCES department(id)
+  ON DELETE SET NULL
+);
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(50) NOT NULL,
+  last_name VARCHAR(50) NOT NULL,
+  role_id INT,
+  manager_id INT,
+);
--- server.js
+++ server.js
@@ -11,3 +11,18 @@ const connection = mysql.createConnection({
     database: "employee_db",
 });
 
+// THEN I am presented with the following options: view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
+
+// THEN I am presented with a formatted table showing department names and department ids
+
+// THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
+
+// THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
+
+// THEN I am prompted to enter the name of the department and that department is added to the database
+
+// THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
+
+// THEN I am prompted to enter the employee’s first name, last name, role, and manager and that employee is added to the database
+
+// THEN I am prompted to select an employee to update and their new role and this information is updated in the database
